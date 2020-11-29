e74a1d0d6cf3a5b7bba603e2b17344744bd3a5ae
Joseph Thomas <jsthomas@protonmail.com>
2020-11-29 10:12:10 -0700

    Update to python 3.

--- README.md
+++ README.md
@@ -1,25 +1,22 @@
 # mgptree
 
-A CLI tool for making "family trees" from the Mathematics Genealogy Project.
+A CLI tool for making "family trees" from the [Mathematics Genealogy Project](https://genealogy.math.ndsu.nodak.edu/).
 
 ## What is this?
 
-`mgptree` allows you to automatically collect data from the
-Mathematics Genealogy project and convert that data to format `dot`
-can consume to produce a directed graph of advisor-advisee
-relationships (similar to a family tree). You can see an extended
-example
-[here](https://jsthomas.github.io/math-genealogy-visualizer.html).
+The Mathematics Genealogy Project records advisor-advisee relationships between mathematicians, 
+going back hundreds of years. `mgptree` allows you to plot these relationships to produce a directed graph,
+similar to a family tree. You can see an extended example [here](https://jsthomas.github.io/math-genealogy-visualizer.html).
 
 ## Install
 
-You will need to install [`graphviz`](http://www.graphviz.org/), and
+This tool uses Python 3.7.3. You will need to install [`graphviz`](http://www.graphviz.org/), and
 possibly `virtualenv` as well (depending on where you prefer to
 install your python libraries). Here is how I set up:
 
     $ virtualenv venv
     $ venv/bin/activate
-    (venv) $ pip install requirements.txt
+    (venv) $ pip install -r requirements.txt
     (venv) $ python mgptree.py -h
 
 This should show you the command line syntax for the tool.
@@ -27,7 +24,7 @@ This should show you the command line syntax for the tool.
 ## Usage
 
 This tool has two modes, "scraping" and "plotting". In scraping mode,
-you specify an input list of names, and a number of "generations" to
+you specify an input list of names and a number of "generations" to
 search backward starting from those names (10-20 is a reasonable
 depth). A typical input file looks like:
 
@@ -39,11 +36,25 @@ Fourier, Jean-Baptiste, Joseph
 (In terms of formatting, the program expects one name per line,
 ordered _last_, _first_, _middle_, where the middle name is optional.)
 
-
-This produces a "database" (pickled python object file) of records
+Scraping produces a pickled python object file of records
 about different mathematicians. In plotting mode, the tool takes this
-database as input and a number of generations, and produces a dotfile
-for that many generations back from the leaves. Generally, scraping is
+file as input and a number of generations, and produces a dotfile
+for that many generations back from the roots (your original names). Scraping is typically
 more time consuming than plotting. Specifying the number of
 generations during plotting lets you see more or less of your dataset,
 without having to do an expensive re-scrape.
+
+## Example Session
+
+Let's build a tree for Alan Turing.
+
+```
+echo "Turing, Alan, Mathison" > names.txt
+python mgptree.py --scrape --generations 10 --input names.txt --output turing.mgp
+python mgptree.py --plot --generations 10 --input turing.mgp --output turing.dot
+dot -Tpng turing.dot -o turing.png
+```
+
+If all went well, `turing.png` should look like this:
+
+![A picture of Turing's advisor tree.](https://github.com/jsthomas/mgptree/blob/master/turing.png?raw=True)
--- mgptree.py
+++ mgptree.py
@@ -1,49 +1,58 @@
 """A CLI tool for scraping the mathematics genealogy project and generating family trees."""
 
 import logging
-import optparse
+import argparse
 import re
 import sys
 import pickle
 import textwrap
-import requests
+from typing import Tuple, List, Optional
 
+import requests
+import urllib3
+
+# Note: The MGP site appears to have a problem with its certificate that triggers SSL exceptions in requests.
+# Since security/privacy is not, in the author's opinion, a significant concern for this application,
+# SSL warnings will be suppressed and the requests library will be configured to skip certificate validation.
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p')
+                    format="%(asctime)s %(message)s",
+                    datefmt="%m/%d/%Y %I:%M:%S %p")
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 GRAPH_FORMAT_STRING = "digraph G{\n node[width = 0.5 fontname=Courier shape=rectangle]\n %s}"
 
+
 def build_opt_parser():
     """Construct a CLI option parser for the application."""
-    parser = optparse.OptionParser()
-    parser.add_option('--scrape', '-s', dest="scrape_on", action="store_true",
-                      default=False, help="Recover data from the MGP.")
-    parser.add_option('--plot', '-p', dest="graph_on", action="store_true",
-                      default=False, help="Use graphviz to draw a genealogy.")
-    parser.add_option('--generations', '-g', dest="gen_depth", default=3,
-                      help="Determines how many generations into the past to search/print.")
-    parser.add_option('--input', '-i', dest="input_file", default=None)
-    parser.add_option('--output', '-o', dest="output_file", default=None)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--scrape", "-s", dest="scrape_on", action="store_true",
+                        default=False, help="Recover data from the MGP.")
+    parser.add_argument("--plot", "-p", dest="graph_on", action="store_true",
+                        default=False, help="Use graphviz to draw a genealogy.")
+    parser.add_argument("--generations", "-g", dest="gen_depth", default=3,
+                        help="Determines how many generations into the past to search/print.")
+    parser.add_argument("--input", "-i", dest="input_file", default=None)
+    parser.add_argument("--output", "-o", dest="output_file", default=None)
 
     return parser
 
 
 def main():  # pylint: disable=missing-docstring
     parser = build_opt_parser()
-    options, _ = parser.parse_args()
+    args = parser.parse_args()
 
-    if options.scrape_on:
-        names = validate_scrape(parser, options)
-        nodes = scrape(names, int(options.gen_depth))
-        pickle_graph_ds(nodes, options.output_file)
+    if args.scrape_on:
+        names = validate_scrape(parser, args)
+        nodes = scrape(names, int(args.gen_depth))
+        pickle_graph_ds(nodes, args.output_file)
 
-    elif options.graph_on:
-        nodes = validate_graph(parser, options)
-        nodes_txt = graph(nodes, int(options.gen_depth))
-        write_graph_text(nodes_txt, options.output_file)
+    elif args.graph_on:
+        nodes = validate_graph(parser, args)
+        nodes_txt = graph(nodes, int(args.gen_depth))
+        write_graph_text(nodes_txt, args.output_file)
 
     else:
         sys.stderr.write("Error: You must select either -s or -p.\n")
@@ -61,43 +70,43 @@ def validate_scrape(parser, options):
         parser.print_help()
         sys.exit(1)
     try:
-        namefile = open(options.input_file)
+        namefile = open(options.input_file, "rb")
     except IOError:
         sys.stderr.write("Error: Input file %s does not exist.\n" % options.names_file)
         parser.print_help()
         sys.exit(1)
 
-    text = namefile.read().decode('utf8')
-    names = [parse_name(line) for line in text.split('\n') if len(line) > 0]
+    text = namefile.read().decode("utf8")
+    names = [parse_name(line) for line in text.split("\n") if len(line) > 0]
     namefile.close()
 
     return names
 
 
-def parse_name(namestring):
+def parse_name(name_line: str) -> Tuple[str, str, str]:
     """Normalize a name string to a name tuple."""
 
-    if ',' in namestring:
-        comma = namestring.index(',')
-        last = (namestring[0:comma]).strip().lower()
-        namestring = (namestring[comma:]).replace(',', '')
-        names = [last] + [name.strip().lower() for name in namestring.split()]
+    if "," in name_line:
+        comma = name_line.index(",")
+        last = (name_line[0:comma]).strip().lower()
+        name_line = (name_line[comma:]).replace(",", "")
+        names = [last] + [name.strip().lower() for name in name_line.split()]
 
     else:
-        namestring = namestring.replace(',', '')
-        names = [name.strip().lower() for name in namestring.split()]
+        name_line = name_line.replace(",", "")
+        names = [name.strip().lower() for name in name_line.split()]
 
     if len(names) == 2:
-        return (names[0], names[1], "")
+        return names[0], names[1], ""
     if len(names) >= 3:
-        return (names[0], names[1], names[2])
+        return names[0], names[1], names[2]
     else:
-        sys.stderr.write(("Error: Only detected a single name in string %s. " + \
-                          "Please provide a first and last name.\n") % namestring)
+        sys.stderr.write(("Error: Only detected a single name in string %s. " +
+                          "Please provide a first and last name.\n") % name_line)
         sys.exit(1)
 
 
-def scrape(names, gens):
+def scrape(names: List[Tuple[str, str, str]], gens: int) -> dict:
     """Scrape N generations of records from the MGP website.
 
     Arguments:
@@ -118,15 +127,15 @@ def scrape(names, gens):
     return node_dict
 
 
-def pickle_graph_ds(nodes, filename):
+def pickle_graph_ds(nodes: dict, filename: str):
     """Persist a dictionary of tree nodes to disk."""
     if filename is None:
         filename = "database.mgp"
-    pickle.dump(nodes, open(filename, 'wb'))
-    sys.stdout.write('Saved %d records to file %s.\n' % (len(nodes), filename))
+    pickle.dump(nodes, open(filename, "wb"))
+    sys.stdout.write("Saved %d records to file %s.\n" % (len(nodes), filename))
 
 
-def validate_graph(parser, options):
+def validate_graph(parser, options) -> dict:
     """Consume and validate user input options for graphing."""
     if options.input_file is None:
         sys.stderr.write("Error: You must provide a saved database as input.\n")
@@ -134,7 +143,7 @@ def validate_graph(parser, options):
         sys.exit(1)
 
     try:
-        nodes = pickle.load(open(options.input_file, 'rb'))
+        nodes = pickle.load(open(options.input_file, "rb"))
     except IOError:
         sys.stderr.write("Error: Could not read file %s.", options.input_file)
         parser.print_help()
@@ -145,11 +154,11 @@ def validate_graph(parser, options):
 
 def graph(nodes, gen_depth):
     """Emit dot code for nodes below a certain generation."""
-    node_string = ""
-    for _, node in nodes.iteritems():
+    node_strings = set()
+    for _, node in nodes.items():
         if node.gen <= gen_depth:
-            node_string += node.dot_string(node.gen != gen_depth)
-    return node_string
+            node_strings.add(node.dot_string(node.gen != gen_depth))
+    return "".join(node_strings)
 
 
 def write_graph_text(node_text, filename):
@@ -159,12 +168,12 @@ def write_graph_text(node_text, filename):
     if filename is None:
         sys.stdout.write(fulltext)
     else:
-        outfile = open(filename, 'w')
-        outfile.write(fulltext.encode('utf8'))
+        outfile = open(filename, "wb")
+        outfile.write(fulltext.encode("utf8"))
         outfile.close()
 
 
-def same_name(name1, name2):
+def same_name(name1: Tuple[str, str, str], name2: Tuple[str, str, str]):
     """Test if two name tuples are equivalent."""
     last1 = name1[0].lower()
     last2 = name2[0].lower()
@@ -173,17 +182,18 @@ def same_name(name1, name2):
     return last1 == last2 and first1 == first2
 
 
-def fetch_id_num(last, first, middle):
+def fetch_id_num(last: str, first: str, middle: str) -> Optional[int]:
     """Attempt to determine the MGP primary key for the input name."""
-    logger.info("Searching MGP for primary key. last=%s, first=%s, middle=%s",
-                last, first, middle)
+    logger.info("Searching MGP for primary key. last=%s, first=%s, middle=%s", last, first, middle)
 
-    url = 'https://genealogy.math.ndsu.nodak.edu/query-prep.php'
-    values = {'given_name': first,
-              'family_name': last,
-              'other_names': middle}
+    url = "https://genealogy.math.ndsu.nodak.edu/query-prep.php"
+    values = {
+        "given_name": first,
+        "family_name": last,
+        "other_names": middle
+    }
 
-    text = (requests.post(url, values)).text
+    text = (requests.post(url, values, verify=False)).text
 
     matches = re.findall(r'<tr><td><a href="id.php\?id=(\d+)">(.+)</a></td>', text)
     pairs = [(int(pair[0]), parse_name(pair[1])) for pair in matches]
@@ -204,7 +214,7 @@ def fetch_id_num(last, first, middle):
     return ids[0]
 
 
-class Node(object):
+class Node:
     """Nodes record all data collected on a particular mathematician.
 
     Typically this information comes from entries in the
@@ -218,22 +228,23 @@ class Node(object):
 
     # pylint: disable=too-many-instance-attributes
 
-    def __init__(self, id_number, gen, max_gen, node_dict):
-        node_dict[id_number] = self
+    def __init__(self, id_number: int, gen: int, max_gen: int, node_data: dict):
+        node_data[id_number] = self
         self.id_num = id_number
         self.gen = gen
 
+        self.title = ""
         self.name = ""
         self.year_of_doctorate = ""
         self.institution = ""
         self.nationality = ""
-        self.advisors = []
+        self.advisors = set()
 
         url = "https://genealogy.math.ndsu.nodak.edu/id.php?id=%d" % id_number
-        text = requests.get(url).text
+        text = requests.get(url, verify=False).text
 
         self.extract_personal_data(text)
-        logger.info("Recovered record: %s %s %s %s\n",
+        logger.info("Recovered record: %s %s %s %s",
                     self.name, self.title, self.institution, self.year_of_doctorate)
 
         if gen >= max_gen:
@@ -246,22 +257,22 @@ class Node(object):
             return
 
         txt_stop = text.find("Student")
-        adv_text = text[txt_start : txt_stop]
+        adv_text = text[txt_start: txt_stop]
 
         advs = [int(id_string)
-                for id_string in re.findall(r'id.php\?id=(\d+)', adv_text)]
+                for id_string in re.findall(r"id.php\?id=(\d+)", adv_text)]
 
         for adv in advs:
-            if adv in node_dict:
-                self.advised_by(node_dict[adv])
+            if adv in node_data:
+                self.advised_by(node_data[adv])
             else:
-                advisor = Node(adv, gen + 1, max_gen, node_dict)
+                advisor = Node(adv, gen + 1, max_gen, node_data)
                 self.advised_by(advisor)
 
     def extract_personal_data(self, text):
         """Search the input web page text for bio data."""
 
-        results = re.findall(r'<title>(.+) - The Mathematics Genealogy Project</title>', text)
+        results = re.findall(r"<title>(.+) - The Mathematics Genealogy Project</title>", text)
         if results:
             self.name = results[0]
 
@@ -275,18 +286,16 @@ class Node(object):
 
     def advised_by(self, other_node):
         """Record an advisor-advisee relationship with another node."""
-        self.advisors.append(other_node)
+        self.advisors.add(other_node)
 
     def dot_string(self, print_advs, brief=True):
         """Emit a dot description for this record."""
-
-        node_str = ""
         if brief:
-            name = textwrap.fill(self.name, 20).replace('\n', '\\n')
+            name = textwrap.fill(self.name, 20).replace("\n", "\\n")
             node_str = "node%d[label=\"%s\\n%s\"];\n" % \
                        (self.id_num, name, self.year_of_doctorate)
         else:
-            institute = textwrap.fill(self.institution, 25).replace('\n', '\\n')
+            institute = textwrap.fill(self.institution, 25).replace("\n", "\\n")
             node_str = "node%d[label=\"%s\\n%s\\n%s\"];\n" % \
                        (self.id_num, self.name, institute, self.year_of_doctorate)
         if print_advs:
@@ -296,5 +305,5 @@ class Node(object):
         return node_str
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
